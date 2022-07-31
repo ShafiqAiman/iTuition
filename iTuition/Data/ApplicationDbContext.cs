8116ed42dfc25daf29294ec13f520f10b6746f9a
Shafiq Aiman <shafiqaiman14261@gmail.com>
2022-07-31 17:34:24 +0800

    Add project files.

---
+++ iTuition/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using iTuition.Models;
+
+namespace iTuition.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<iTuition.Models.Student>? Student { get; set; }
+    }
+}
---
+++ iTuition/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿namespace iTuition.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public string Package { get; set; }
+
+        public string School { get; set; }
+
+        public Student()
+        {
+
+        }
+    }
+}
